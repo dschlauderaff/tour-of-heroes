be0c4bfca5284bff28517fc0c984a745e0189730
Dave Schlauderaff <dschlauderaff@inspirus.com>
2019-11-08 11:50:46 -0600

    http requests and search

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../../models/hero';
-import { HEROES } from '../../mock-heroes';
-import { HeroService } from '../../services/hero.service';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../../models/hero';
+import {HeroService} from '../../services/hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,17 +9,31 @@ import { HeroService } from '../../services/hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  constructor(private heroService: HeroService) {}
+
+  constructor(private heroService: HeroService) {
+  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  // getHeroes(): void {
-  //   this.heroService.getHeroes().subscribe(heroes => {
-  //     this.heroes = heroes;
-  //   });
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 }
